6de235b754d8603eb94cdabf75a2d37134d79f46
Amadeus Mader <amadeus@mozart409.com>
2026-04-26 21:33:16 +0200

    feat: add act for gh workflow testing

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           # keep-sorted start
+          act
           bacon
           cargo-deny
           cargo-workspaces
